ab9ea75e277c37dd1952d2810b419fa53d70efc3
gutsytechster <prashantsharma161198@gmail.com>
2018-07-10 18:36:19 +0530

    refactor: Added View and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
 	path('', views.post_list, name='post_list'),
+	path('post/<int:kp>/', views.post_detail, name='post_detail'),
+	# re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
