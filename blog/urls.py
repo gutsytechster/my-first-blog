f2e39dc3c1141d8340e28a871f975b7c29e8dca7
gutsytechster <prashantsharma161198@gmail.com>
2018-07-20 18:26:09 +0530

    refactor: Change support by draft post and login

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
 	path('post/<int:kp>/', views.post_detail, name='post_detail'),
 	path('post/new/', views.post_new, name='post_new'),	
 	path('post/<int:kp>/edit', views.post_edit, name='post_edit'),
+	path('draft/', views.post_draft_list, name='post_draft_list'),
+	path('post/<int:kp>/publish', views.post_publish, name='post_publish'),
+	path('post/<int:kp>/remove', views.post_remove, name='post_remove'),
 	# re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
